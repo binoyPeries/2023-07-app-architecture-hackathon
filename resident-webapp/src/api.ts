972f11b70e46477d9e9be2c53dd9bc3d081ffaa5
binoyPeries <binoy.18@cse.mrt.ac.lk>
2023-08-02 09:46:56 +0530

    api updated

--- resident-webapp/src/api.ts
+++ resident-webapp/src/api.ts
@@ -1,13 +1,13 @@
-import axios from 'axios'
-import oauth from 'axios-oauth-client'
+import axios from "axios";
+import oauth from "axios-oauth-client";
 
-let accessToken: string = '';
+let accessToken: string = "";
 
 export function setAccessToken(token: string) {
-    accessToken = token;
+  accessToken = token;
 }
 
 export const API = axios.create({
-    baseURL: 'https://1698d406-65dc-49be-b615-ab2451a002b1-dev.e1-us-east-azure.choreoapis.dev/qfxg/visit-service/visit-420/1.0.0'
-   
+  baseURL:
+    "https://2c6c3c6e-fa65-4e8d-8943-215214887ec8-dev.e1-us-east-azure.choreoapis.dev/wlxf/visitapi/visit-420/1.0.0",
 });
